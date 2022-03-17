5b9da90bb2fab2f2811df0fcfc4a047b404c8bf2
BeignetPA <22002963@etu.unicaen.fr>
2022-03-17 15:28:09 +0100

    Mise en place de la route index La route par default où l'on peut trouver un formulaire brut

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -1,7 +1,7 @@
-import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import Abstractroute from './abstractroute';
 
-export default class EmployeesRoute extends Route {
+export default class EmployeesRoute extends Abstractroute {
   @service store;
   model() {
     return this.store.findAll('employee');
