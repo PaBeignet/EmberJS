f9388236b012a185c0401a821d045ceb050d59e3
BeignetPA <22002963@etu.unicaen.fr>
2022-03-25 16:29:41 +0100

    Commit de 16h30 Bug lors de la validation de order

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class OrderRoute extends Route {
   @service store;
@@ -8,5 +9,15 @@ export default class OrderRoute extends Route {
     return this.store.findRecord('order', params.order_id, {
       include: 'user,orderdetails,orderdetails.product',
     });
+  }
+
+  @action
+  save(idOrder, montant) {
+    this.store.findRecord('order', idOrder).then(function (order) {
+      order.toPay = montant;
+      order.status = 'prepared';
+      order.save();
+    });
+    this.transitionTo('board');
   }
 }
